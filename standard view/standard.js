8041217a1f71a4c9b4a2ba8abc5bd8cf5389f7aa
Chase Robinson <srobinson6@mocs.flsouthern.edu>
2022-08-21 19:51:30 -0400

    added routing & backgrounds

--- standard view/standard.js
+++ standard view/standard.js
@@ -1,1 +1,54 @@
 // This is where the standard site functions will go
+
+
+function hideAllRoutes() {
+    //remove active class from nav-link in header
+    var link = $("a.pt-link.active")
+    link.removeClass("active")
+
+    //remove page-active page from the sections
+    var page = $("section.page-active")
+    page.removeClass("page-active")
+}
+
+window.addEventListener('hashchange', function() {
+    hideAllRoutes();
+    var page;
+    var link;
+    switch(location.hash) {
+        case "#about":
+            link = $("a[href='#about']")
+            
+            page = $("section#about")
+            break;
+        case "#resume":
+            link = $("a[href='#resume']")
+            
+            page = $("section#resume")
+            break;
+        case "#portfolio":
+            link = $("a[href='#portfolio']")
+            
+            page = $("section#portfolio")
+            break;
+        case "#blog":
+            link = $("a[href='#blog']")
+            
+            page = $("section#blog")
+            break;
+        case "#contact":
+            link = $("a[href='#contact']")
+            
+            page = $("section#contact")
+            break;
+        default:
+            link = $("a[href='#home']")
+            
+            page = $("section#home")
+            break;
+    }
+    // add active class to nav-link in header
+    link.addClass("active")
+    //add page-active to section
+    page.addClass("page-active")
+});
